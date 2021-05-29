5d6e4f45de6ec6d8856f3e10d49c6ee4d6d9b0c1
Bloodcloak <59000966+bloodcloak@users.noreply.github.com>
2021-05-29 01:05:36 -0500

    initial test

--- listingPing/listingPing.py
+++ listingPing/listingPing.py
@@ -1,53 +1,118 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.utils import get
 from core import checks
 from core.models import PermissionLevel
+from datetime import datetime, timedelta
+import logging
+
+logger = logging.getLogger("Modmail")
 
 class listingPing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.pingChannel = self.bot.get_channel(int('847687831823974440'))
+        self.guild = self.bot.get_guild(int('842915739111653376'))
         self.db = bot.api.get_plugin_partition(self)
+        self.timeDelta = timedelta(minutes=1)
+        self.msgQueue = dict()
 
         self.monitorChannels = ('842969358355529728', '842933135654387732', '846142270881005668', '842933105996070953', '842933116213002272', '842933126296895519', '842933147209170975', '842933075826704394', '842933044494336011', '842933058221899787', '842933067500617728', '842933083707932703')
 
-    # self.bot.get_channel(int(self.pingChannel))
+    async def _updateDB(self):
+        await self.db.find_one_and_update(
+            {"_id": "msgQueue"}, {"$set": {"msgQueue": self.msgQueue}}, upsert=True
+        )
+
+    @tasks.loop(seconds=30) #@tasks.loop(minutes=5)
+    async def handleQueue(self):
+        currTime = datetime.now().timestamp()
+        for msgID, obj in self.msgQueue.items():
+            if obj["rmTime"] > currTime:
+                # Time not elapsed, skip to next item
+                continue
+            # MSG Due for Ping
+            
+            ## Determine which role to ping
+            pingRole = None
+            chanIdx = self.monitorChannels.index(obj["chanID"])
+
+            if chanIdx == 0:
+                pingRole = "Map Seller"
+            elif chanIdx == 1:
+                pingRole = "PC Mod Seller"
+            elif chanIdx >= 2 and chanIdx <= 6:
+                pingRole = "PC Asset Seller"
+            elif chanIdx == 7:
+                pingRole = "Quest Mod Seller"
+            elif chanIdx >= 8 and chanIdx <= 11:
+                pingRole = "Quest Asset Seller"
+            else:
+                logger.error("Invalid Indexing Occured")
+                continue
+
+            ## Process and Ping
+            role = get(self.guild.roles, name=pingRole)
+            user = self.bot.fetch_user(int(obj["usrID"]))
+            channel = self.bot.get_channel(int(obj["chanID"]))
+            self.pingChannel.send(f"{role.mention} \n{user} posted a listing in {channel.mention}!\nLink: https://discord.com/channels/842915739111653376/{channel.id}/{msgID}")
+
+            # Pop the entry out
+            res = self.msgQueue.pop(str(msgID), None)
+            await self._updateDB()
+            logger.info("Ping Occured for:", msgID, "\nUser: ", user, "\nResult: \n", res)
+
+    @handleQueue.before_loop
+    async def _setDB(self):
+        await self.bot.wait_until_ready()
+        msgQueue = await self.db.find_one({"_id": "msgQueue"})
+
+        if msgQueue is None:
+            await self.db.find_one_and_update(
+                {"_id": "msgQueue"}, {"$set": {"msgQueue": dict()}}, upsert=True
+            )
+
+            msgQueue = await self.db.find_one({"_id": "msgQueue"})
+        
+        self.msgQueue = msgQueue.get("msgQueue", dict())
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
         if ctx.author.bot: return
         
         if str(ctx.channel.id) in self.monitorChannels:
-            # Register message for time delay queue
-            obStore = {}
-            obStore["msgID"] = int(ctx.message.id)
-            obStore["chanID"] = int(ctx.channel.id)
+            validMsg = True
+            keywords = ('status:', 'payment:', 'budget:')
+            lowCase = ctx.content.lower()
+
+            for keyword in keywords:
+                if lowCase.find(keyword) == -1:
+                    validMsg = False
+                    logger.info("Message Failed Store: ", lowCase)
+
+            if validMsg:
+                # Register message for time delay queue
+                rmTimeCal = datetime.now() + self.timeDelta
+
+                obStore = {}
+                obStore["usrID"] = int(ctx.author.id)
+                obStore["msgID"] = int(ctx.message.id)
+                obStore["chanID"] = int(ctx.channel.id)
+                obStore["rmTime"] = rmTimeCal.timestamp()
+                self.msgQueue[str(ctx.message.id)] = obStore
+                await self._updateDB()
+                logger.info("Message Triggered Store: \n", obStore)
+                return
             
     @commands.Cog.listener()
     async def on_message_delete(self, ctx):
         if ctx.author.bot: return
         
         if str(ctx.channel.id) in self.monitorChannels:
-            # Check and remove from the queue
-
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    @commands.command()
-    async def setPingChannel(self, ctx, channel: discord.TextChannel):
-        """Sets the media log channel"""
-        await self.db.find_one_and_update({'_id': 'config'}, {'$set': {'log_channel': str(channel.id), 'monitored_channels': []}}, upsert=True)
-        await ctx.send('Success')
-
-    @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    @commands.command()
-    async def setMonitor(self, ctx, channel: discord.TextChannel, rolename = None):
-        """Sets the channel to monitor for pings"""
-        if await self.is_monitored(channel):
-            await self.db.find_one_and_update({'_id': 'config'}, {'$pull': {'monitored_channels': str(channel.id)}}, upsert=True)
-            await ctx.send('Removed Channel From Monitor')
-        else:
-            await self.db.find_one_and_update({'_id': 'config'}, {'$addToSet': {'monitored_channels': str(channel.id)}}, upsert=True)
-            await ctx.send('Added Channel To Monitor')
+            # Remove from the queue if exists
+            res = self.msgQueue.pop(str(ctx.message.id), None)
+            await self._updateDB()
+            logger.info("Message Deleted: \n", ctx.message.id, "\nResult: \n", res)
         
     async def cog_command_error(self, ctx, error):
         """Checks errors"""
